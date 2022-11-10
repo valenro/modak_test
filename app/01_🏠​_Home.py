31e53c74debc279ba8b936f39e9f3e67467a1743
valenro <valenproomg@gmail.com>
2022-11-10 06:23:32 -0300

    app

---
+++ app/01_🏠​_Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.markdown(
+    '''
+    # MODAK - Data Analytics Capabilities Test
+
+    In the following pages you will see the resolution for this test.
+
+    You can consult the source code at this github repository
+    * https://github.com/valenro/modak_test
+    To solve exercise number 2 I created a python class which is stored in this route:
+    * modak_test/app/utils/class_analytics.py
+    '''
+)
